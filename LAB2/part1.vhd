031b1c71f8a49a0a19381f8be23b12d2fb6b2458
95766963 <49899319+95766963@users.noreply.github.com>
2020-02-07 18:53:10 -0800

    Add files via upload

--- LAB2/part1.vhd
+++ LAB2/part1.vhd
@@ -9,52 +9,67 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.numeric_std.ALL;
 
+
 entity ParityCheck is
      generic( width : integer := 8 );
      port( input : in std_logic_vector( width-1 downto 0 );
            output: out std_logic );
 end entity ParityCheck;
 
---Non recursive xor tree
+
+
+
 architecture Tree1 of ParityCheck is
-	signal iq : std_logic_vector( 7 downto 0);
+	signal iq : std_logic_vector( 7 downto 0) ;
 begin
-	output <= iq(7);
-	A0: entity work.xor2 port map (input(0), input(1), iq(1));
-	A1: entity work.xor2 port map (input(2), input(3), iq(2));
-	A2: entity work.xor2 port map (iq(1), iq(2), iq(3));
-	A3: entity work.xor2 port map (input(4), input(5), iq(4));
-	A4: entity work.xor2 port map (input(6), input(7), iq(5));
-	A5: entity work.xor2 port map (iq(4), iq(5), iq(6));
-	A6: entity work.xor2 port map (iq(3), iq(6), iq(7));
+
+	output <= iq(6);
+	A0: entity work.xor2 port map( input(0), input(1),iq(0) );
+	A1: entity work.xor2 port map( input(2), input(3),iq(1) );
+	A2: entity work.xor2 port map( input(4), input(5),iq(2) );
+	A3: entity work.xor2 port map( input(6), input(7),iq(3) );
+	A4: entity work.xor2 port map( iq(0), iq(1),iq(4) );
+	A5: entity work.xor2 port map( iq(2), iq(3),iq(5) );
+	A6: entity work.xor2 port map( iq(4), iq(5),iq(6) );
+
+
+
+
+
 end architecture tree1;
 
---Non recursive xor chain
+
+
+
 architecture Chain1 of ParityCheck is
-	signal iq : std_logic_vector( 7 downto 0);
+	signal iq : std_logic_vector( 7 downto 0) ;
 begin
 	iq(0) <= input(0);
 	output <= iq(7);
-	A0: entity work.xor2 port map (iq(0), input(1), iq(1));
-	A1: entity work.xor2 port map (iq(1), input(2), iq(2));
-	A2: entity work.xor2 port map (iq(2), input(3), iq(3));
-	A3: entity work.xor2 port map (iq(3), input(4), iq(4));
-	A4: entity work.xor2 port map (iq(4), input(5), iq(5));
-	A5: entity work.xor2 port map (iq(5), input(6), iq(6));
-	A6: entity work.xor2 port map (iq(6), input(7), iq(7));
+	A0: entity work.xor2 port map( iq(0), input(1),iq(1) );
+	A1: entity work.xor2 port map( iq(1), input(2),iq(2) );
+	A2: entity work.xor2 port map( iq(2), input(3),iq(3) );
+	A3: entity work.xor2 port map( iq(3), input(4),iq(4) );
+	A4: entity work.xor2 port map( iq(4), input(5),iq(5) );
+	A5: entity work.xor2 port map( iq(5), input(6),iq(6) );
+	A6: entity work.xor2 port map( iq(6), input(7),iq(7) );
+	
+
+
 end architecture chain1;
 
 
 
---Repeately generated xor chain
 architecture Chain2 of ParityCheck is
 	signal iq : std_logic_vector( 7 downto 0 );
 begin
 	iq(0) <= input(0);
 	output <= iq(7);
-gg: for z in 0 to 6 generate
-Az: entity work.xor2 port map ( iq(z), input(z+1), iq(z+1) );
+gg:	for z in 0 to 6 generate
+Az:	entity work.xor2 port map ( iq(z), input(z+1), iq(z+1) );
 	end generate gg;
+
+
 end architecture chain2;
 
 
@@ -68,17 +83,31 @@ end architecture chain2;
 -- The division by 2 truncates so you must be very careful when dividing the
 -- circuit into two sub-circuits and indexing their input arrays.
 -----------------------------------------------------------------------------
+--architecture Tree2 of ParityCheck is
+	---signal iq : std_logic_vector( 7 downto 0 );
+--begin
+--loop:
+--loop1: 
+--Az:	
+       -- if z >= 3 and z<=z/2;
+      --  entity work.xor2 port map( input(z),input(z+1),iq(z) );
+	--end generate loop1;
+--loop2:	
+       -- if z >= 3
+        
+--Az2:	entity work.xor2 port map( input(z),input(z+1),iq(z) );
+	--end generate loop1;
+--end architecture Tree2;
 architecture Tree2 of ParityCheck is
 	signal Eup, Elo : std_logic;
 begin
   Recur: if ( width > 1 ) generate
 	Upper: Entity work.ParityCheck(Tree2) generic map( (width+1)/2 ) port map(input( width-1 downto width/2), Eup);
 	Lower: Entity work.ParityCheck(Tree2) generic map( (width)/2 ) port map(input( width/2 - 1 downto 0), Elo);
-	output <= Eup xnor Elo;
+	output <= Eup xor Elo;
 	End Generate Recur;
 
  StopRecur: if width = 1 generate
-	output <= not input(0);
+	output <=  not input(0);
 	End Generate StopRecur;
 end architecture Tree2;
-
